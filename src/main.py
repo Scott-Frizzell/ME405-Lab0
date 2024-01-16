943f75ed333ad0d1638fad099067842c81a19b3c
Scott-Frizzell <scott.frizzell@hotmail.com>
2024-01-16 15:43:13 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-"""! @file main.py
-
+"""!
+@file main.py
+Tests input and output of the Nucleo board with a capacitive step response.
 """
 
 import pyb
@@ -10,37 +11,39 @@ pinC0.low() # Set C0 to low
 adc0 = pyb.ADC(pyb.Pin.board.PB0) # Initialize B0 as ADC input
 timer = pyb.Timer(1, freq=100) # Initialize timer channel 1 to 100Hz (10ms)
 timer.callback(None) # Set TC1 to have no callback
-int_queue = cqueue.IntQueue(200) # Initialize a queue to store ADC readings
+queue = cqueue.FloatQueue(200) # Initialize a queue to store ADC readings
 
 def step_response():
     """!
-    
+    Enables timer interrupts and begins a step response. After the response is done, prints each value.
     @returns None
     """
-    pinC0.high() # Set C0 to high
     timer.counter(0) # Reset TC1 counter
     timer.callback(timer_int) # Set TC1 callback to timer_int()
+    pinC0.high() # Set C0 to high
     
-    while not int_queue.full(): # Wait until queue is full
+    while not queue.full(): # Wait until queue is full
         pass
     
-    timer.callback(None) # Clear TC1 callback
-    
-    for i in range(int_queue.available()):
-        print("{:d},{:d}".format(i * 10, int_queue.get())) # For each reading in the queue, print comma-separated time and value
+    for i in range(queue.available()):
+        print(f"{(i * 10):2f},{queue.get():2f}") # For each reading in the queue, print comma-separated time and value
     
     print("End") # Print end to indicate function is finished
-    
+
+    pinC0.low() # Reset C0 to low
+
     return
 
 def timer_int(channel):
     """!
-    
+    Puts the value from adc0 into the queue. If the queue is full, it removes the callback function.
     @param channel The timer channel that called the function
     @returns None
     """
     if channel == 1:
-        int_queue.put(adc0.read())
+        queue.put(adc0.read())
+    if  queue.full():
+        timer.callback(None) # Clear TC1 callback
     return
 
 if __name__ == "__main__":
